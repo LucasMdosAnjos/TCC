a8b0ebce0f68b64cc219551988a7efbaf10b4217
LucasMdosAnjos <lucasmon@live.com>
2022-09-27 11:51:21 -0300

    ajustes

--- pneumonia_flutter/lib/app/constants/config_constants.dart
+++ pneumonia_flutter/lib/app/constants/config_constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
 
 class ConfigConstants{
-  static String hostnameFlask = kReleaseMode ? 'https://8eaa-2804-14c-87c4-8471-c44b-f5f2-f8f3-3b53.sa.ngrok.io' : 'http://192.168.0.18:5000';
+  static String hostnameFlask = kReleaseMode ? 'https://8eaa-2804-14c-87c4-8471-c44b-f5f2-f8f3-3b53.sa.ngrok.io' : 'http://192.168.201.118:5000';
 }
